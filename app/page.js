e95e93d8da5ae2d550375b19062b901685f1ff66
Jeet Paul <jeetpaul2222@gmail.com>
2026-03-30 11:44:12 +0530

    feat: initial commit

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
-const page = () => {
-  return <div>page</div>;
-};
+import Contact_form from '@/components/Contact_form'
+import React from 'react'
 
-export default page;
+const page = () => {
+  return (
+    <div>
+      <div>
+        <h1>Server action Demo</h1>
+        <p>Connect form with MongoDB & revalidation</p>
+      </div>
+      <Contact_form />
+    </div>
+  )
+}
+
+export default page
---
+++ components/Contact_form.jsx
@@ -0,0 +1,9 @@
+"use client"
+
+const Contact_form = () => {
+  return (
+    <div>Contact_form</div>
+  )
+}
+
+export default Contact_form
